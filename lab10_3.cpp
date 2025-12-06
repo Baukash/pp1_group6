735cd41518dfb648af6ad19c847632a0a7430b42
Baukash <156778292+Baukash@users.noreply.github.com>
2025-12-06 18:20:45 +0500

    Create lab10_3.cpp

---
+++ lab10_3.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+struct Point{
+  int x;
+  int y;
+};
+
+int comp(Point p1, Point p2){
+  return p1.y<p2.y;
+}
+
+
+int main() 
+{
+    vector<Point> v;
+    int n;
+    cin>>n;
+    
+    for(int i=0 ;i<n; i++){
+      int x;
+      int y;
+      cin>>x>>y;
+      Point p;
+      p.x = x;
+      p.y = y;
+      v.push_back(p);
+    }
+    
+    sort(v.begin(), v.end(), comp);
+    
+    
+    for(Point i:v){
+      cout<<i.x<<" "<<i.y<<endl;
+    }
+    
+    
+}
+
+
+//4
+//1 4
+//2 3
+//1 6
+//3 -1
